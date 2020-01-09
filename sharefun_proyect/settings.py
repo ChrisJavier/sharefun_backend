e92ddf040bd3e3db5078378b2b008a8e8a19ebea
Christian Carrera <christian.carrera@tipti.com.ec>
2020-01-08 22:23:44 -0500

    Changes in wgsi field

--- sharefun_proyect/settings.py
+++ sharefun_proyect/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sharefun_proyect.urls'
@@ -128,3 +129,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
