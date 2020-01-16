710b69db504cfc05a6407cb618ba62667ab4dd82
Christian Carrera <christian.carrera@tipti.com.ec>
2020-01-15 23:35:41 -0500

    update setting.py

--- sharefun_proyect/settings.py
+++ sharefun_proyect/settings.py
@@ -71,13 +71,9 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
-
 WSGI_APPLICATION = 'sharefun_proyect.wsgi.application'
 
 # Database
